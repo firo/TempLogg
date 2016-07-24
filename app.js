418b1a5f7e5b9e048c669467e7f8e37bb824587e
Ferruccio Irionè <mail@firo.it>
2016-07-24 15:16:44 +0200

    add binfing port by env

--- app.js
+++ app.js
@@ -1,7 +1,29 @@
 var express = require('express');
 var app = express();
 var json = { status: '' };
+app.locals.data = [];
 
+// rules for defining the response and light color
+app.use('/:id/:temp', function (req, res, next) {
+  if (req.params.temp < 20 ) {
+    json.status = 'GREEN';
+  } else if (req.params.temp < 23) {
+    json.status = 'YELLOW';
+  } else if (req.params.temp >= 23) {
+    json.status = 'RED';
+  }
+  next();
+});
+
+// cache the income data
+app.use('/:id/:temp', function (req, res, next) {
+  // do some action if income temperature is diffrence from previous one
+  if (req.params.temp != app.locals.data[req.params.id]) {
+      app.locals.data[req.params.id] = req.params.temp;
+      console.log('updated locals data');
+  }
+  next();
+});
 
 // logger for temperature by device id
 app.use('/:id/:temp', function (req, res, next) {
@@ -16,4 +38,8 @@ app.get('/:id/:temp', function (req, res) {
   res.json(json);
 });
 
-app.listen(3000);
+var server = app.listen(process.env.PORT, function () {
+  var host = server.address().address;
+  var port = server.address().port;
+  console.log('Web server started at http://%s:%s', host, port);
+});
