2c41c98f5c31b04bad848e8a5fa9a63e79635a84
Ferruccio Irionè <mail@firo.it>
2016-07-24 14:51:00 +0200

    Initial commit

---
+++ app.js
@@ -0,0 +1,41 @@
+var express = require('express');
+var app = express();
+var json = { status: '' };
+app.locals.data = [];
+
+// rules for defining the response and light color
+app.use('/log/:id/:temp', function (req, res, next) {
+  if (req.params.temp < 20 ) {
+    json.status = 'GREEN';
+  } else if (req.params.temp < 23) {
+    json.status = 'YELLOW';
+  } else if (req.params.temp >= 23) {
+    json.status = 'RED';
+  }
+  next();
+});
+
+// cache the income data
+app.use('/log/:id/:temp', function (req, res, next) {
+  // do some action if income temperature is diffrence from previous one
+  if (req.params.temp != app.locals.data[req.params.id]) {
+      app.locals.data[req.params.id] = req.params.temp;
+      console.log('updated locals data');
+  }
+  next();
+});
+
+// logger for temperature by device id
+app.use('/log/:id/:temp', function (req, res, next) {
+  console.log('-> device id: ' + req.params.id + ' temperature: ' + req.params.temp);
+  next();
+});
+
+// The handler function (middleware system).
+// The function handles GET requests to the /user/:id path.
+app.get('/log/:id/:temp', function (req, res) {
+  res.type('json');               // => 'application/json'
+  res.json(json);
+});
+
+app.listen(3000);
