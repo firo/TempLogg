8c43c1e0c5ab784d2fa5edf50178fc8d69fdef8e
Ferruccio Irionè <mail@firo.it>
2016-08-01 15:23:45 +0200

    add socket.IO and monitor realtime

--- app.js
+++ app.js
@@ -1,9 +1,18 @@
-var express = require('express');
-var bodyParser = require('body-parser');
-var app = express();
-var json = { status: '' };
+var express   =     require("express");
+var app       =     express();
+var http 	    = 	require('http').Server(app);
+var io 		    = 	require('socket.io')(http);
+
 app.locals.data = [];
 
+var env = process.env.NODE_ENV || 'development';
+var json = { status: '' };
+
+app.use('/js',express.static( __dirname + '/client/js'));
+app.use('/style',express.static( __dirname + '/client/css'));
+
+
+// --> Express Middleware
 // rules for defining the response and light color
 app.use('/:id/:temp', function (req, res, next) {
   if (req.params.temp < 20 ) {
@@ -16,7 +25,7 @@ app.use('/:id/:temp', function (req, res, next) {
   next();
 });
 
-// cache the income data
+// rules for chaching data
 app.use('/:id/:temp', function (req, res, next) {
   // do some action if income temperature is diffrence from previous one
   if (req.params.temp != app.locals.data[req.params.id]) {
@@ -32,19 +41,21 @@ app.use('/:id/:temp', function (req, res, next) {
 // logger for temperature by device id
 app.use('/:id/:temp', function (req, res, next) {
   console.log('-> device id: ' + req.params.id + ' temperature: ' + req.params.temp);
+  // emit temperature to all sockets connected
+  var date = new Date().getTime();
+  io.sockets.emit('temperatureUpdate', date, req.params.temp);
   next();
 });
 
-
-app.use(bodyParser.raw());
-
 // The handler function (middleware system).
-// The function handles GET requests to the /user/:id path.
+// The function handles GET requests to the /:id/:temp path.
 app.get('/:id/:temp', function (req, res) {
   res.type('json');               // => 'application/json'
-  res.json(json);
+  res.json(json);                 // => send json back to client
 });
 
+
+// Manage the income outbout messages
 app.post('/closedcase',function(req,res) {
   //req.setEncoding('utf8');
   console.log('soap post in call');
@@ -52,8 +63,14 @@ app.post('/closedcase',function(req,res) {
   res.end("ok");
 });
 
-var server = app.listen(process.env.PORT || 3000, function () {
-  var host = server.address().address;
-  var port = server.address().port;
-  console.log('Web server started at http://%s:%s', host, port);
+// routing for homepage
+app.get('/', function(req, res){
+	//res.send('id: ' + req.query.id);
+  res.sendFile(__dirname + '/client/index.html');
+});
+
+// Express
+var port = process.env.PORT || 3000; // Use the port that Heroku provides or default to 5000
+http.listen(port, function(){
+  console.log("Express server listening on port %d in %s mode", port, app.settings.env);
 });
