17882c509db83a4dc4f559fa339befd0416fb761
Ferruccio Irionè <mail@firo.it>
2016-11-22 14:43:30 +0100

    fix routing path and connecting dots

--- app.js
+++ app.js
@@ -10,6 +10,8 @@ app.locals.refdelta = 3;
 
 var conn = new jsforce.Connection();
 var env = process.env.NODE_ENV || 'development';
+var debug = process.env.DEBUG || false;
+var SFStatus = process.env.SFDC_STATUS || false;
 var json = { status: '' };
 var username = process.env.SFDC_UID;
 var password = process.env.PWD + process.env.SFDC_TOKEN;
@@ -31,15 +33,16 @@ app.use('/:temp', function (req, res, next) {
 });
 
 // logger for temperature and emit temperature to all sockets connected
-app.use('/:temp', function (req, res, next) {
-  //console.log('-> temperature: ' + req.params.temp);
+app.use('/t/:temp', function (req, res, next) {
+  if (debug) console.log('routing /temp logger');
+  if (debug) console.log('--> temperature: ' + req.params.temp);
   var date = new Date().getTime();
   io.sockets.emit('temperatureUpdate', date, req.params.temp); // dashboard web
   next();
 });
 
 // logger for temperature by device id and emit temperature to all sockets connected
-app.use('/track/:device/:temp', function (req, res, next) {
+app.use('/t/:device/:temp', function (req, res, next) {
   //console.log('-> temperature: ' + req.params.temp);
   var date = new Date().getTime();
   var channel = 'tempUpd-' + req.params.device;
@@ -49,53 +52,59 @@ app.use('/track/:device/:temp', function (req, res, next) {
   res.json(json);
 });
 
-// compute the data analytics
-app.use('/:temp', function (req, res, next) {
+// compute the data analytics and call SFDC
+app.use('/t/:temp', function (req, res, next) {
   app.locals.data.push(req.params.temp);
-  if (app.locals.data.length > 10){
-    // console.log('enought data');
-    var max = Math.max.apply(null, app.locals.data);
-    var min = Math.min.apply(null, app.locals.data);
-    delta = max - min;
-    io.sockets.emit('messageUpdate', 'Current delta is '+ delta + ' ('+ min + '-'+ max + ')');
-    // console.log('max: ' + max + ' min: '+ min);
-    if ( delta > 3 ) {
-      //console.log('OPEN CASE');
+  if (process.env.SFDC = true)
+    if (app.locals.data.length > 10){
+      // console.log('enought data');
+      var max = Math.max.apply(null, app.locals.data);
+      var min = Math.min.apply(null, app.locals.data);
+      delta = max - min;
+      io.sockets.emit('messageUpdate', 'Current delta is '+ delta + ' ('+ min + '-'+ max + ')');
+      // console.log('max: ' + max + ' min: '+ min);
+      if ( delta > 3 ) {
+        //console.log('OPEN CASE');
+        if (SFStatus) {
+          conn.login(username, password, function(err, userInfo) {
+            if (err) { return console.error(err); }
+            var records = [];
+            // Single record creation
+            conn.sobject("Case").create({ Subject : 'Temperature too high',
+                                          Description : 'the device Frigo17B has reached a too high temperature with a greater increase in the average of all similar devices. '+
+                                          'Current temperature is ' + req.params.temp + 'C with the increase of ' + delta + ' C.',
+                                          ContactId : '0035800000FqZqgAAF',
+                                          Origin : 'IoT',
+                                          Priority : 'Medium',
+                                          Status :  'New',
+                                          SuppliedName : 'Frigo17B' }, function(err, ret) {
+              if (err || !ret.success) { return console.error(err, ret); }
+              //console.log("Created record id : " + ret.id);
+              io.sockets.emit('messageUpdate', 'Case created id: '+ ret.id);
+            });
 
-      conn.login(username, password, function(err, userInfo) {
-        if (err) { return console.error(err); }
-        var records = [];
-        // Single record creation
-        conn.sobject("Case").create({ Subject : 'Temperature too high',
-                                      Description : 'the device Frigo17B has reached a too high temperature with a greater increase in the average of all similar devices. '+
-                                      'Current temperature is ' + req.params.temp + 'C with the increase of ' + delta + ' C.',
-                                      ContactId : '0035800000FqZqgAAF',
-                                      Origin : 'IoT',
-                                      Priority : 'Medium',
-                                      Status :  'New',
-                                      SuppliedName : 'Frigo17B' }, function(err, ret) {
-          if (err || !ret.success) { return console.error(err, ret); }
-          //console.log("Created record id : " + ret.id);
-          io.sockets.emit('messageUpdate', 'Case created id: '+ ret.id);
-        });
+            conn.logout(function(err) {
+              if (err) { return console.error(err); }
+              // now the session has been expired.
+            });
 
-        conn.logout(function(err) {
-          if (err) { return console.error(err); }
-          // now the session has been expired.
-        });
+          });
+        } // end if SFStatus
+      }
+    } else {
+      if (SFStatus) {
+        io.sockets.emit('messageUpdate', 'not enough data...');
+      } else {
+        io.sockets.emit('messageUpdate', 'SFDC interaction off');
+      }
 
-      });
-    }
-  } else {
-    io.sockets.emit('messageUpdate', 'not enough data...');
-    console.log('need more data');
   }
   next();
 });
 
 // The handler function (middleware system).
 // The function handles GET requests to the /:temp path.
-app.get('/:temp', function (req, res) {
+app.get('/t/:temp', function (req, res) {
   res.type('json');               // => 'application/json'
   res.json(json);                 // => send json back to client
 });
@@ -109,12 +118,13 @@ app.get('/delta/:var', function (req, res) {
 
 // routing for homepage
 app.get('/', function(req, res){
+  if (debug) console.log('routing home');
 	//res.send('id: ' + req.query.id);
   res.sendFile(__dirname + '/client/index.html');
 });
 
 // routing for homepage by device
-app.get('/rtm/:device', function(req, res){
+app.get('/d/:device', function(req, res){
   res.sendFile(__dirname + '/client/index_by_device.html');
 });
 
